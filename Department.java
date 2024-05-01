4fd8c466a3fd8feee30bf57eb353e5a8c09109e2
Daniela <daniela.dupusamezgaile@gmail.com>
2024-05-01 09:10:34 +0300

    finished department class

--- Department.java
+++ Department.java
@@ -1,3 +1,14 @@
+import java.util.List;
+
 public class Department {
-    
+    String name;
+    List<Employee> employees;
+
+    public void addEmployee(Employee employee){
+        this.employees.add(employee);
+    }
+
+    public void displayEmployeesTable(){
+        System.out.println("Total number of employees in " + this.name + " department: " + employees.size());
+    }
 }
