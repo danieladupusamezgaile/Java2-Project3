96176635f8ee7d608f1f8875517650fd7885fe69
Daniela <daniela.dupusamezgaile@gmail.com>
2024-05-01 09:05:38 +0300

    finishec employee class

--- Employee.java
+++ Employee.java
@@ -1,4 +1,12 @@
 public class Employee {
+    int employeeID;
+    String name;
 
-    
+    public int getID(){
+        return this.employeeID;
+    }
+
+    public String getName(){
+        return this.name;
+    }
 }
